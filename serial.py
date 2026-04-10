f472458ff1eab5695d97bec38ee61aa47cfb888c
vitor zamir josé do nascimento <vitorzamirzamir@gmail.com>
2026-04-10 12:10:48 -0300

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,1 +1,33 @@
+#!/usr/bin/env pybricks-micropython
 
+# Classe para controlar os servos da placa de servos via porta serial
+class Servos:
+    def __init__(self, portaServos, atualizaInstantaneo = False):
+        self.lista = [0xff, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
+        self.ser = portaServos
+        self.atualizaInstantaneo = atualizaInstantaneo
+
+    def move(self,servo,angulo):
+        if(servo <= 0):
+            return
+        if(servo > 6):
+            return
+        if(angulo < 0):
+            angulo = 0
+        if(angulo > 180):
+            angulo = 180
+        self.lista[servo] = angulo
+        if self.atualizaInstantaneo:
+            self.atualiza()
+
+    def atualiza(self):
+        self.ser.write(bytes(self.lista))
+
+    def desativa(self,servo):
+        if(servo <= 0):
+            return
+        if(servo > 6):
+            return
+        self.lista[servo] = 200 #maior que 180 desativa ele
+        if self.atualizaInstantaneo:
+            self.atualiza()
