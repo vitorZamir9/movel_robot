5074039fd5b81c7137e2a631875e0e1d3d578c01
vitorZamir9 <vitorzamirzamir@gmail.com>
2026-05-22 17:19:37 -0300

    Mudança na classe de curva preto

--- Gabi_code/Update_Segu/black909.py
+++ Gabi_code/Update_Segu/black909.py
@@ -9,7 +9,20 @@ class Black909:
         self.ev3 = ev3
 
     def blackORwhite(self, fora1, meio1, meio2, fora2, pretoesq, pretodir):
-        if pretodir > 0: 
+        self.motorB.run(-100)
+        self.motorC.run(-100)
+        retorno = self.sensor1.read(0)
+        while True:
+            retorno = self.sensor1.read(0)
+            fora1 = retorno[3]#esquerda 
+            meio1 = retorno[2]#esquerda 
+            meio2 = retorno[1]#direita  
+            fora2 = retorno[0]#direita
+            if (fora1,meio1,meio2,fora2) < 40:
+                self.tanki.stop()
+                break
+        wait(100)
+        if pretodir > 0 and fora2 < 40: 
             print("90preto esquerda")
             self.tanki.turn(10)
             self.tanki.stop() 
@@ -21,11 +34,11 @@ class Black909:
             self.motorB.dc(-100)
             self.motorC.dc(-100)
             retorno = self.sensor1.read(0)
-            fora2 = retorno[3] #direita 
+            fora2 = retorno[0] #direita 
             wait(100)
             while True:
                 retorno = self.sensor1.read(0)
-                fora2 = retorno[3] #direita  
+                fora2 = retorno[0] #direita  
                 wait(100)
                 print(fora2)
                 if fora2 <= 50:
@@ -41,7 +54,7 @@ class Black909:
             #tsttttttttttttttttttt direitaaaaaaaaaaaa
             #tsttttttttttttttttttt direitaaaaaaaaaaaa
             
-        elif pretoesq > 0:
+        elif pretoesq > 0 and fora1 < 40:
             print("90preto direita")
             self.tanki.turn(10)
             self.tanki.stop() 
@@ -53,11 +66,11 @@ class Black909:
             self.motorB.dc(100)
             self.motorC.dc(100)
             retorno = self.sensor1.read(0)
-            fora1 = retorno[0] #esquerda 
+            fora1 = retorno[3] #esquerda 
             wait(100)
             while True:
                 retorno = self.sensor1.read(0)
-                fora1 = retorno[0] #esquerda 
+                fora1 = retorno[3] #esquerda 
                 wait(100)
                 print(fora1)
                 if fora1 <= 50:
@@ -78,7 +91,32 @@ class Black909:
             # GAP
             self.ev3.speaker.beep(1000)
             print("gaap")
-            
+            self.motorB.run(-100)
+            self.motorC.run(-100)
+            retorno = self.sensor1.read(0)
+            while True:
+                retorno = self.sensor1.read(0)
+                fora1 = retorno[3]#esquerda 
+                meio1 = retorno[2]#esquerda 
+                meio2 = retorno[1]#direita  
+                fora2 = retorno[0]#direita
+                if (fora1,meio1,meio2,fora2) < 40:
+                    self.tanki.stop()
+                    break
+            wait(100)
+            self.motorB.run(100)
+            self.motorC.run(100)
+            retorno = self.sensor1.read(0)
+            while True:
+                retorno = self.sensor1.read(0)
+                fora1 = retorno[3]#esquerda 
+                meio1 = retorno[2]#esquerda 
+                meio2 = retorno[1]#direita  
+                fora2 = retorno[0]#direita
+                if (fora1,meio1,meio2,fora2) < 40:
+                    self.tanki.stop()
+                    break
+            wait(100)
             #fazer com que o robo agora va para o outro lado
             #ou seja fazer com que o robo va para frente ate ver a linha preta
             #utilizar os sensores fora1,meio1,meio2,fora2 para poder identificar a linha
