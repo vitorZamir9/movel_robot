45ed1416f1bfb251595ed883b458017b469e9f3e
NewAtom <newatom16@gmail.com>
2026-07-07 16:00:01 -0300

    black corrigido

--- Gabi_fullcode/Update_Segu/black909.py
+++ Gabi_fullcode/Update_Segu/black909.py
@@ -12,71 +12,48 @@ class Black909:
         self.gap = Gapwhite(tanki, motorB, motorC, sensor1, ev3)
 
     def blackORwhite(self, fora1, meio1, meio2, fora2, pretoesq, pretodir):
-        if pretoesq > 0 : 
+        if pretodir > 0 : 
             print("90preto esquerda")
-            #self.tanki.turn(5)
-            self.tanki.stop() 
-            self.motorB.stop()
-            self.motorC.stop()
-            wait(100)
-            self.ev3.speaker.beep()
-            self.motorB.dc(-100)
-            self.motorC.dc(-100)
-            self.motorB.dc(-100)
-            self.motorC.dc(-100)
-            retorno = self.sensor1.read(2)
-            fora2 = retorno[0] #direita 
-            wait(100)
             while True:
+                self.motorB.dc(80)
+                self.motorC.dc(80)
                 retorno = self.sensor1.read(2)
-                fora2 = retorno[3] #direita  
-                wait(100)
-                print(fora2)
-                if fora2 <= 50:
-                    self.tanki.stop()
+                meio2 = retorno[1] #direita  
+                
+                print(meio2)
+                if meio2 <= 50:
+                    self.motorB.stop()
+                    self.motorC.stop()
                     pretodir = 0
                     pretoesq = 0
                     break
-            print("fez")
-            wait(100)
-            self.motorB.stop()
-            self.motorC.stop() 
-            self.tanki.stop()   
-            #tsttttttttttttttttttt direitaaaaaaaaaaaa
-            #tsttttttttttttttttttt direitaaaaaaaaaaaa
-            #tsttttttttttttttttttt direitaaaaaaaaaaaa
             
-        elif pretodir > 0 :
+            print("fez preto esquerda")
+            wait(100)
+            #tsttttttttttttttttttt direitaaaaaaaaaaaaa
+            #tsttttttttttttttttttt direitaaaaaaaaaaaaa
+            #tsttttttttttttttttttt direitaaaaaaaaaaaaa
+            
+            
+            
+        elif pretoesq > 0 :
             print("90preto direita")
-            #self.tanki.turn(5)
-            self.tanki.stop() 
-            self.motorB.stop()
-            self.motorC.stop()
-            wait(100)
-            self.ev3.speaker.beep()
-            self.motorB.dc(100)
-            self.motorC.dc(100)
-            self.motorB.dc(100)
-            self.motorC.dc(100)
-            retorno = self.sensor1.read(2)
-            fora1 = retorno[3] #esquerda 
-            wait(100)
             while True:
+                self.motorB.dc(-80)
+                self.motorC.dc(-80)
                 retorno = self.sensor1.read(2)
-                fora1 = retorno[0] #esquerda 
-                wait(100)
-                print(fora1)
-                if fora1 <= 50:
-                    self.tanki.stop()
+                meio1 = retorno[2] #direita  
+                
+                print(meio1)
+                if meio1 <= 50:
+                    self.motorB.stop()
+                    self.motorC.stop()
                     pretodir = 0
                     pretoesq = 0
-                    contGap=0
                     break
-            print("fez")
+            
+            print("fez preto direita")
             wait(100)
-            self.motorB.stop()
-            self.motorC.stop()
-            self.tanki.stop()
             #tsttttttttttttttttttt esquerdaaaaaaaaaaaaa
             #tsttttttttttttttttttt esquerdaaaaaaaaaaaaa
             #tsttttttttttttttttttt esquerdaaaaaaaaaaaaa
