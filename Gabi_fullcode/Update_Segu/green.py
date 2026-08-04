c60bb9e91554a87dddaca8a56c4a34adc2cd8e39
NewAtom <newatom16@gmail.com>
2026-08-04 13:29:02 -0300

    atualização verde

--- Gabi_fullcode/Update_Segu/green.py
+++ Gabi_fullcode/Update_Segu/green.py
@@ -14,14 +14,42 @@ class Green:
         #self.motores = Segue(motorB, motorC, PESO_FORA, PESO_MEIO)
     
     def MoveGreen(self,H1, S1, V1, H2, S2, V2, H3, S3, V3, alvo,
-                   fora1, meio1, meio2, fora2, previsao_camera, cloresq, clordir, pretoesq, pretodir):
-        
+                   fora1, meio1, meio2, fora2, cloresq, clordir, pretoesq, pretodir):
+        #=====================================
+        # 0.0 ATUALIZAÇÃO DOS VALORES DOS SENSORES
+        #=====================================
+        retorno = self.sensor1.read(2)
+        # Leitura dos sensores para seguir linha
+        fora1 = retorno[3] # esquerda 
+        meio1 = retorno[2] # esquerda 
+        meio2 = retorno[1] # direita  
+        fora2 = retorno[0] # direita  
+        # Leitura da posição do sensor sobre a linha preta
+        posicao = (retorno[29]*2)
+        # Leitura unitária dos sensores de cor
+        cloresq = retorno[17]
+        clormind = retorno[18]
+        clordir = retorno[19]
+        # Leitura RGBC dos sensores
+        R1, R3, R2 = (retorno[4]), (retorno[8]), (retorno[12])
+        G1, G3, G2 = (retorno[5]), (retorno[9]), (retorno[13])
+        B1, B3, B2 = (retorno[6]), (retorno[10]), (retorno[14])
+        C1, C3, C2 = (retorno[7]), (retorno[11]), (retorno[15])
+        # Leitura HSV para o verde
+        H1, H3, H2 = (retorno[20]*2), (retorno[23]*2), (retorno[26]*2)
+        S1, S3, S2 = (retorno[21]*2), (retorno[24]*2), (retorno[27]*2)
+        V1, V3, V2 = (retorno[22]*2), (retorno[25]*2), (retorno[28]*2)
+        alvo = 10 # Alvo para a calibração do HSV do verde
         #=====================================
         #BLOCOS DE VERDES
         #=====================================
-        
         # r1 b1 g1, h1 s1 v1, cloresq
-        
+
+        #verde0 foi oq eu usei no regional e estadual
+        verdeDireita0 = H1 >=(95-alvo) and H1 <=(140+alvo) and S1 >=(47-alvo) and S1 <=(73+alvo) and V1 >=(40-alvo) and V1 <=(80+alvo)
+        verdeMeio0 = H3 >=(95-alvo) and H3 <=(140+alvo) and S3 >=(47-alvo) and S3 <=(73+alvo) and V3 >=(40-alvo) and V3 <=(80+alvo)
+        verdeEsquerda0 = H2 >=(95-alvo) and H2 <=(140+alvo) and S2 >=(47-alvo) and S2 <=(73+alvo) and V2 >=(40-alvo) and V2 <=(80+alvo)
+
         verdeDireita1 =  H1 >=(110-alvo) and H1 <=(110+alvo) and S1 >=(64-alvo) and S1 <=(72+alvo) and V1 >=(72-alvo) and V1 <=(74+alvo)
         verdeMeio1 = H3 >=(110-alvo) and H3 <=(110+alvo) and S3 >=(64-alvo) and S3 <=(72+alvo) and V3 >=(72-alvo) and V3 <=(74+alvo)
         verdeEsquerda1 = H2 >=(110-alvo) and H2 <=(110+alvo) and S2 >=(60-alvo) and S2 <=(60+alvo) and V2 >=(86-alvo) and V2 <=(86+alvo)
@@ -34,217 +62,47 @@ class Green:
         verdeMeio3 = H3 >=(110-alvo) and H3 <=(110+alvo) and S3 >=(75-alvo) and S3 <=(75+alvo) and V3 >=(100-alvo) and V3 <=(100+alvo)
         verdeEsquerda3 = H2 >=(110-alvo) and H2 <=(110+alvo) and S2 >=(60-alvo) and S2 <=(60+alvo) and V2 >=(80-alvo) and V2 <=(80+alvo)
 
-        verdeDireita = verdeDireita1 or verdeDireita2 or verdeDireita3
-        verdeMeio = verdeMeio1 or verdeMeio2 or verdeMeio3
-        verdeEsquerda = verdeEsquerda1 or verdeEsquerda2 or verdeEsquerda3
-
-        if not (verdeDireita or verdeEsquerda or verdeMeio or previsao_camera != None):
-            return previsao_camera
-            
+        verdeDireita = verdeDireita0 or verdeDireita1 or verdeDireita2 or verdeDireita3
+        verdeMeio = verdeMeio0 or verdeMeio1 or verdeMeio2 or verdeMeio3
+        verdeEsquerda = verdeEsquerda0 or verdeEsquerda1 or verdeEsquerda2 or verdeEsquerda3
         # ==================================
-        # LÓGICA DA DIREITA
+        # LÓGICA DA PRINCIPAL
         # ==================================
-        #wait(100)
-        #print("verde")
-        if verdeDireita and not pretodir > 0:
-
-            
-            if verdeEsquerda: #detectou dois verdes, é beco
-                wait(10)
-                self.tanki.stop()
-                self.ev3.speaker.beep(600, 300) 
-                self.ev3.speaker.beep(100, 100) 
-                print(">>> EXECUTANDO BECO")
-                self.tanki.turn(30)
-                self.tanki.straight(190)
-                self.tanki.stop()
-                self.motorB.dc(100)
-                self.motorC.dc(100)
-                while True:
-                    retorno = self.sensor1.read(2)
-                    m1 = retorno[2]
-                    if m1 <= 40:
-                        self.tanki.stop()
-                        break
-                self.tanki.stop()
-                
-                self.motorB.stop()
-                self.motorC.stop()
-                self.tanki.turn(-50)
-                self.tanki.stop()
-                return None
-            
-            #self.ser.write(b"passou_verde\n")
-            #se não for beco, segue a lógica normal
-            #if meio1 >= 40 or meio2 >= 40:
-            else: #verificar possíveis falhas de movimentação
-                self.tanki.stop()
-                self.tanki.turn(60)
-                self.tanki.straight(90)
-                self.ev3.speaker.beep(200,100)
-                self.tanki.stop()
-                self.ev3.speaker.beep(200,1000) 
-                print(">>> EXECUTANDO VERDE DIREITA")
-                self.tanki.stop()
-                self.motorB.dc(90)
-                self.motorC.dc(90)
-                while True:
-                    retorno = self.sensor1.read(2)
-                    m1 = retorno[2]
-                    if m1 <= 50:
-                        self.tanki.stop()
-                        break
-                
-                self.motorB.stop()
-                self.motorC.stop()
-                return None 
-                
-            return None 
-
-        # ==================================
-        # LÓGICA DA ESQUERDA
-        # ==================================
-        elif verdeEsquerda and not pretoesq > 0:
-            wait(10)
-
-            if verdeDireita: #detectou dois verdes, é beco
-                wait(10)
-                self.tanki.stop()
-                self.ev3.speaker.beep(600, 300) 
-                self.ev3.speaker.beep(100, 100) 
-                print(">>> EXECUTANDO BECO")
-                self.tanki.turn(30)
-                self.tanki.straight(190)
-                self.tanki.stop()
-                self.motorB.dc(90)
-                self.motorC.dc(90)
-                while True:
-                    retorno = self.sensor1.read(2)
-                    m1 = retorno[2]
-                    if m1 <= 50:
-                        self.tanki.stop()
-                        break
-                self.tanki.stop()
-                
-                self.motorB.stop()
-                self.motorC.stop()
-                self.tanki.turn(-50)
-                self.tanki.stop()
-                return None
-             
-            #if meio1 >= 40 or meio2 >= 40:
-            else: #verificar possíveis falhas de movimentação
-                self.tanki.stop()
-                self.tanki.turn(60)
-                self.tanki.straight(-90)
-                self.tanki.stop()
-                self.ev3.speaker.beep(200,1000) 
-                print(">>> EXECUTANDO VERDE ESQUERDA")
-                self.tanki.stop()
-                self.motorB.dc(-100)
-                self.motorC.dc(-100)
-                while True:
-                    retorno = self.sensor1.read(2)
-                    m2 = retorno[1]
-                    if m2 <= 40:
-                        self.tanki.stop()
-                        break
-                self.motorB.stop()
-                self.motorC.stop()
-                # print('viu verde e tá parado')
-                # self.tanki.stop()
-                # wait(300000000)
-                #self.ser.write(b"passou_verde\n")
-            return None
-        
-        elif verdeMeio: #foi torto e viu o verde no meio
-            return None
+        if verdeDireita or verdeMeio or verdeEsquerda: 
             self.tanki.stop()
-            self.motorB.stop()
-            self.motorC.stop()
-            self.ev3.speaker.beep(200,100) 
-            print(">>> EXECUTANDO VERDE MEIO")
-            #wait(100)
-            if meio1 < 20: #pra direita
-                self.tanki.turn(30)
-                self.tanki.straight(60)
-                self.ev3.speaker.beep(200,100) 
-                print(">>> EXECUTANDO VERDE MEIO DIREITA")
-                self.tanki.stop()
-                self.motorB.dc(90)
-                self.motorC.dc(90)
-                while True:
-                    retorno = self.sensor1.read(2)
-                    m1 = retorno[2]
-                    if m1 <= 50:
-                        self.tanki.stop()
-                        break
-                
-                self.motorB.stop()
-                self.motorC.stop()
+            # ==================================
+            # LÓGICA DA DIREITA
+            # ==================================
+            if H1 >=(90-alvo) and H1 <=(105+alvo) and S1 >=(50-alvo) and S1 <=(70+alvo) and V1 >=(40-alvo) and V1 <=(80+alvo) and fora1 > meio1 and fora2 > meio2 :
+                wait(30)
+                #utilizando um valor com uma calibração mais suave e que pega uma margem boa de verdes
+                if verdeDireita:
+                    print("fazer logica do verde, partindo da identificação do verde na direita")
+                    self.motorB.stop()
+                    self.motorC.stop()
+                    self.tanki.stop()
+                    self.motorB.reset_angle(0)
+                    self.motorC.reset_angle(0)
+                    # OBS: fazer verficação com motor.run pois a movimentação lenta é escenssial para o verde
+                    # vai pra tras dentro de um while, onde ele vai perguntar se esta vendo verde com o outro sensor atraves de um somatorio,senão ele vai fazer o verde que ele viu primeiro
+                    #
                 return None
-            
-            elif meio2 < 20:#pra esquerda
-                self.tanki.turn(30)
-                self.tanki.straight(-60)
-                self.ev3.speaker.beep(200,100) 
-                print(">>> EXECUTANDO VERDE MEIO ESQUERDA")
-                self.tanki.stop()
-                self.motorB.dc(-100)
-                self.motorC.dc(-100)
-                while True:
-                    retorno = self.sensor1.read(2)
-                    m2 = retorno[1]
-                    if m2 <= 40:
-                        self.tanki.stop()
-                        break
-                self.motorB.stop()
-                self.motorC.stop()
+            # ==================================
+            # LÓGICA DA ESQUERDA
+            # ==================================
+            elif H2 >=(90-alvo) and H2 <=(105+alvo) and S2 >=(50-alvo) and S2 <=(70+alvo) and V2 >=(40-alvo) and V2 <=(80+alvo) and fora1 > meio1 and fora2 > meio2 :
+                wait(30)
+                #utilizando um valor com uma calibração mais suave e que pega uma margem boa de verdes
+                if verdeEsquerda:
+                    print("fazer logica do verde, partindo da identificação do verde na esquerda")
                 return None
-            else:
-                self.tanki.turn(-50)
+            # ==================================
+            # LÓGICA DO MEIO
+            # ==================================
+            elif H3 >=(90-alvo) and H3 <=(110+alvo) and S3 >=(50-alvo) and S3 <=(70+alvo) and V3 >=(40-alvo) and V3 <=(80+alvo): #foi torto e viu o verde no meio
+                wait(30)
+                #utilizando um valor com uma calibração mais suave e que pega uma margem boa de verdes
+                if verdeMeio:
+                    print("fazer logica do verde, partindo da identificação do verde no meio")
                 return None
-
-        # ==================================
-        # LÓGICA DO BECO #MANTER SÓ PRA DIZER QUE TEM
-        # ==================================
-        # elif (verdeDireita and verdeEsquerda):
-        #     wait(10)
-        #     self.tanki.stop()
-        #     self.ev3.speaker.beep(600) 
-        #     print(">>> EXECUTANDO BECO")
-        #     self.tanki.turn(30)
-        #     self.tanki.straight(190)
-        #     self.tanki.stop()
-            
-        #     self.motorB.stop()
-        #     self.motorC.stop()
-        #     self.tanki.turn(-50)
-        #     self.tanki.stop()
-            
-        #     #self.ser.write(b"passou_verde\n")
-        #     return None 
-        
-
-        
-        # ==================================
-        # LÓGICA DE GAP (DEPOIS)
-        # ==================================
-        # elif (meio1 <= 30 or meio2 <= 30) and (cloresq == 1 or clordir == 1): #pode dar errado?
-        #     self.tanki.stop()
-        #     self.ev3.speaker.beep(800, 200) 
-        #     print(">>> SEGUINDO POR TEMPO (GAP/DEPOIS)")
-        #     cronometro = StopWatch()
-        #     tempo_limite = 500  
-        #     while cronometro.time() < tempo_limite:
-        #         retorno = self.sensor1.read(2)
-        #         f1, m1, m2, f2 = retorno[0], retorno[1], retorno[2], retorno[3]
-                
-        #         # AQUI É ONDE USAMOS O SEU PID:
-        #         self.motores.PID(f1, m1, m2, f2, 2.0, 0, 0.15, 100)
-        #         wait(10)
-                
-        #     print(">>> TEMPO ESGOTADO: Voltando ao loop principal")
-        #     return None
-
-        return previsao_camera
+        return None
